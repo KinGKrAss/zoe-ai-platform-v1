e0f8ae0a4bc42074c734a9dc5d0ead3acdea9b1c
Rene Demir geborener Enstone <89888045+KinGKrAss@users.noreply.github.com>
2026-08-15 16:29:15 +0200

    feat(z1): expose Z1 URI primitives

---
+++ services/z1-core/__init__.py
@@ -0,0 +1,9 @@
+from .z1_uri import Z1Resolver, Z1URI, Z1URIError, parse_z1_uri, resolve_z1_reference
+
+__all__ = [
+    "Z1Resolver",
+    "Z1URI",
+    "Z1URIError",
+    "parse_z1_uri",
+    "resolve_z1_reference",
+]
