eacdc42927513b8d9704e7ab647f598f95a17aed
Rene Demir geborener Enstone <89888045+KinGKrAss@users.noreply.github.com>
2026-08-20 03:46:43 +0200

    refactor: remove non-importable CMC package path

--- services/zoe-connectors/coinmarketcap/__init__.py
+++
@@ -1,5 +0,0 @@
-"""CoinMarketCap connector for ZOE-CORE."""
-
-from .client import CoinMarketCapClient, CoinMarketCapError, market_assets
-
-__all__ = ["CoinMarketCapClient", "CoinMarketCapError", "market_assets"]
